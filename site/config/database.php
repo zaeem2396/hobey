637842048cad6514ab5c02a6a3553971f6119e56
zaeem2396 <zaeem.ansari@varvali.in>
2021-09-23 17:05:41 +0530

    added env.php file

--- site/config/database.php
+++ site/config/database.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
+require_once FCPATH . 'env.php';
 /*
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
